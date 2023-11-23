14b84decd57125695623cf436b65f4993a221aa2
Thato Will Joseph <programergeek@gmail.com>
2023-11-23 17:29:26 +0200

    feat: text highlighting

--- init.lua
+++ init.lua
@@ -233,6 +233,10 @@ require('lazy').setup({
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
   },
+  -- Text highlighting
+
+  { 'rrethy/vim-illuminate' },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.

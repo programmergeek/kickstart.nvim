d0c1fbfbdfa15ca8e740fd26cb583963da5e2824
Thato Will Joseph <programergeek@gmail.com>
2024-08-15 09:49:34 +0200

    config: bufferline line style and show diagnostics information

--- init.lua
+++ init.lua
@@ -290,7 +290,14 @@ require('lazy').setup({
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
-      options = { diagnostics = 'nvim_lsp', separator_style = 'slanted' },
+      options = {
+        diagnostics = 'nvim_lsp',
+        separator_style = 'slant',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match 'error' and ' ' or ''
+          return ' ' .. icon .. count
+        end,
+      },
     },
   },
 
